b53b296082ab6dcd12b72ddf0cbe02d052ca1c60
In-range-37 <kprabin126@gmail.com>
2025-10-06 15:45:58 +0545

    Added squares and cube

--- xap1.c
+++ xap1.c
@@ -4,34 +4,43 @@ int main()
 {
     int var1, var2, result;
 
+    char sign;
+    printf("What operation do you want to perform: ");
+    scanf(" %c", &sign);
+
     printf("What is the First Number: ");
     scanf("%d", &var1);
 
     printf("What is the Second Number: ");
     scanf("%d", &var2);
 
-    char sign;
-    printf("Now what do you want to do ?");
-    scanf(" %c", &sign);
 
-    if (sign == '+' ){
+    if (sign == '+' || sign =='A' || sign =='a' ){
         result = var1 + var2;
         printf("The answer is: %d\n", result);
     }
-    else if (sign == '-'){
+    else if (sign == '-' || sign == 'G'|| sign == 'g'){
         result = var1 - var2;
         printf("The answer is: %d\n", result);
     }
-        else if (sign == '*'){
+    else if (sign == '*'|| sign == 'm'|| sign == 'M'){
         result = var1 * var2;
         printf("The answer is: %d\n", result);
     }
-        else if (sign == '/'){
+    else if (sign == '/' || sign == 'D' || sign == 'd'){
         result = var1 / var2;
         printf("The answer is: %d\n", result);
     }
+    else if (sign == 'S' || sign == 's' || sign == '2'){
+        result = var1 * var1;
+        printf("The answer of the square is: %d\n", result);
+    }
+    else if (sign =='C' || sign == 'c' || sign == '3'){
+        result = var1 * var1 * var1;
+        printf("The answer of the cube is: %d\n", result);
+    }
     else{
-        printf("Invalid!!!");
+        printf("Invalid");
     }
 }
 
