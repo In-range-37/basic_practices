d6b6e5cbc28e6d1792bf30ef2b74ef5804f3c029
In-range-37 <kprabin126@gmail.com>
2025-10-18 20:51:08 +0545

    tic tac toe sample

---
+++ tic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, j;
+    int matrix [2] [3] = {{1,2,3}, {4,5,6}};
+
+    printf("\n\n");
+    for (i = 0; i<2; i++){
+        printf("|");
+        for (j = 0; j<3; j++){
+            printf("\t%d\t| ", matrix[i][j]);
+            
+        }
+        printf("\n\n");
+    }
+
+    printf("\n");
+
+}
