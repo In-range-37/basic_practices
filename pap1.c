12cc141b83487b4fd78eafb0bacf8c9b2dfbe260
In-range-37 <kprabin126@gmail.com>
2025-11-02 19:59:06 +0545

    Update pap1.c
    added studies routine

--- pap1.c
+++ pap1.c
@@ -4,7 +4,7 @@ int main() {
   FILE *fptr;
   fptr = fopen("ponk.txt", "w");
 
-  char gym[100], wake [100], hygiene[100];
+  char gym[100], wake [100], hygiene[100], studies[100];
 
   printf("Did you wake before 6 a.m: ");
   scanf("%s", wake);
@@ -15,9 +15,13 @@ int main() {
   printf("Did you bathe: ");
   scanf("%s", hygiene);
 
+  printf("Heading out for studies: ");
+  scanf("%s", studies);
+
   fprintf(fptr, "[%s] Wake before 6 a.m \n", wake);
   fprintf(fptr, "[%s] Gym \n", gym);
   fprintf(fptr, "[%s] Bath and refresh \n", hygiene);
+  fprintf(fptr, "[%s] Headed for studies\n", studies);
 
   fclose(fptr);
   return 0;
