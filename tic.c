dc2276e060b4896139f7beef84b142e4d2264cce
In-range-37 <kprabin126@gmail.com>
2026-01-14 22:55:05 +0545

    Finished prototype of tic tac toe.

--- tic.c
+++ tic.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
+    void askingtoe()
+        {
+        int entry = printf("Number to cross: ");
+        scanf("%i", &entry);
+        }
+
 int main(){
 
-    int i, j;
-    int matrix [2] [3] = {{1,2,3}, {4,5,6}};
+    int i, j, n;
+    int matrix [3] [3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+    printf("\n");
 
-    printf("\n\n");
-    for (i = 0; i<2; i++){
+    
+    for (i = 0; i<3; i++){
         printf("|");
         for (j = 0; j<3; j++){
             printf("\t%d\t| ", matrix[i][j]);
-            
         }
         printf("\n\n");
     }
