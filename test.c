e16422ca8d52c4fce3e96e78553dd74cedddd7a0
In-range-37 <kprabin126@gmail.com>
2026-01-15 22:01:15 +0545

    Asking X and O

--- test.c
+++ test.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
 
-int askingtoe() {
+int askingX() {
     int entry;
     printf("Number to cross: ");
     scanf("%d", &entry);
     return entry;
 }
 
+int askingO(){
+    int entry;
+    printf("Number to round: ");
+    scanf("%d", &entry);
+    return entry;
+}
+
+
+
 int main() {
     int i, j;
     int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
 
-    int choice = askingtoe();
+    int X = askingX();
+    int O = askingO();
+
+    do{
+        printf("Type Again!\n");
+        askingO();
+    }
+    while (X != O);
+
+    printf("\n");
 
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
-            if (matrix[i][j] == choice) {
+            if (matrix[i][j] == X) 
+            {
                 printf("\tX\t");
-            } else {
+            }
+            
+            else if (matrix[i][j] == O)
+            {
+                printf("\tY\t");
+            }
+            
+            else {
                 printf("\t%d\t", matrix[i][j]);
             }
         }
--- tic.c
+++ tic.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 
-    void askingtoe()
-        {
-        int entry = printf("Number to cross: ");
-        scanf("%i", &entry);
-        }
+int askingtoe()
+    {
+    int entry = printf("Number to cross: ");
+    scanf("%i", &entry);
+    return entry;
+    }
 
 int main(){
 
-    int i, j, n;
+    int i, j;
+    int choice = askingtoe();
     int matrix [3] [3] = {{1,2,3}, {4,5,6}, {7,8,9}};
     printf("\n");
 
     
     for (i = 0; i<3; i++){
-        printf("|");
         for (j = 0; j<3; j++){
-            printf("\t%d\t| ", matrix[i][j]);
+            if (matrix[i][j] == choice){
+                printf("\tX\t");
+            }
+            else{
+                printf("\t%d\t ", matrix[i][j]);
+            }
         }
         printf("\n\n");
     }
+   //askingtoe();
 
     printf("\n");
     
